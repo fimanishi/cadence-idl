84c7995f5c7deb7da9e4fa89438c625ae95e8ede
fimanishi <fimanishi@gmail.com>
2026-09-11 15:00:24 -0700

    remove domain name and id, add workflow reuse policy and tasklist
    Signed-off-by: fimanishi <fimanishi@gmail.com>

--- thrift/shared.thrift
+++ thrift/shared.thrift
@@ -2521,15 +2521,14 @@ struct ExportWorkflowExecutionRequest {
   20: optional WorkflowExecution execution
   30: optional i32 pageSize
   40: optional binary nextPageToken
+  50: optional i64 (js.type = "Long") lastEventID
 }
 
 struct ExportWorkflowExecutionResponse {
   10: optional WorkflowExecution execution
   20: optional list<DataBlob> historyBatches
   30: optional VersionHistory versionHistory
-  40: optional string sourceDomainID
-  50: optional string sourceDomainName
-  60: optional binary nextPageToken
+  40: optional binary nextPageToken
 }
 
 struct ImportWorkflowExecutionRequest {
@@ -2537,10 +2536,10 @@ struct ImportWorkflowExecutionRequest {
   20: optional WorkflowExecution execution
   30: optional list<DataBlob> historyBatches
   40: optional VersionHistory versionHistory
-  50: optional string sourceDomainID
-  60: optional string sourceDomainName
-  70: optional binary pageToken
-  80: optional bool forceRestart
+  50: optional binary pageToken
+  60: optional bool forceRestart
+  70: optional WorkflowIdReusePolicy workflowIdReusePolicy
+  80: optional string taskList
 }
 
 struct ImportWorkflowExecutionResponse {
